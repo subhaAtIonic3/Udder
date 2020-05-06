21e915ee8a5b8b1714030646e2ff532d6fe425b6
Subhrajyoti <subha.ionic3@gmail.com>
2020-05-07 02:13:35 +0530

    change view controller

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("inside videDidLoad")
         // Do any additional setup after loading the view.
     }
 
